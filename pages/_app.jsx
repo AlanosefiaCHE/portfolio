b48dad5c2d5d485ad44b115a2f2f3a25f923b7ca
20alan01 <20alan01@gmail.com>
2022-04-26 23:17:01 +0200

    Navbar added

---
+++ pages/_app.jsx
@@ -0,0 +1,23 @@
+
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
+import Head from 'next/head'
+import { useEffect } from "react";
+
+function MyApp({ Component, pageProps }) {
+     
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+}, []);
+  return (
+    <>
+    <Head>
+      
+       <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
+ 
+    <Component {...pageProps} />
+    </>
+    )
+  }
+export default MyApp
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,10 +1,15 @@
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar/Navbar'
 
-export default function Home() {
+export default function About() {
   return (
+    <>
+    <Navbar />
     <h1>about</h1>
-    
+    <button className="btn btn-primary m-3">Button Primary</button>
+    </>
   )
 }
