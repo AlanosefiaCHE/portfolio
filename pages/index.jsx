91cf6218969f2ba49ea22654c2411eead1af963f
20alan01 <20alan01@gmail.com>
2022-04-29 15:08:08 +0200

    Footer component added

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,12 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Navbar/Footer'
 
 export default function Home() {
   return (
     <>
      <Navbar />
        <h1>Home</h1>
+       <p className='text'></p>
+       <p></p>
+      <Footer />
     </>
    
 
