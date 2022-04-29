01b57164e0b26b200241c61f645ec747cb54757f
20alan01 <20alan01@gmail.com>
2022-04-29 15:11:40 +0200

    Footer added to all pages

--- pages/stocks.jsx
+++ pages/stocks.jsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Navbar/Footer'
 
 export default function Stocks() {
   return (
@@ -10,7 +11,7 @@ export default function Stocks() {
     <Navbar />
     <h1>Stocks</h1>
     <h2>Coming soon</h2>
-
+    <Footer />
     </>
   )
 }
