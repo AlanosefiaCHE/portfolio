b4de9d799d425bdf77e9f0819b67091254c41d1f
20alan01 <20alan01@gmail.com>
2022-04-29 15:30:40 +0200

    3 containers for social media logo's

--- components/Navbar/Footer.jsx
+++ components/Navbar/Footer.jsx
@@ -6,12 +6,15 @@ export default function Footer() {
   <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div className="col-md-4 d-flex align-items-center">
       <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-
+      <svg className="bi" width="30" height="24"></svg>
       </a>
       <span className="text-muted">© 2021 Alan Osefia</span>
     </div>
 
-    <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li className="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+      <li className="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
+      <li className="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"></svg></a></li>
 
     </ul>
   </footer>
