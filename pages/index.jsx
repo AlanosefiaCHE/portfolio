f845da6345abb8a6afdb3fde0a3efd4cc84e157c
20alan01 <20alan01@gmail.com>
2022-04-30 14:59:25 +0200

    Home page color changed to blue

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,15 +4,23 @@ import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Navbar/Footer'
 
+
 export default function Home() {
   return (
     <>
       <Navbar />
-      <h1>Home</h1>
+     
       <div className='hometext container'>
         <div>
+          <div className='logo-homepage'>
+             <Image src="/logo.svg" alt="me" width="50" height="50" /> 
+          </div>
+      
           <h1 className='text'>Welcome to my website</h1>
-          <p className='text'>My name is Alan Osefia  and i'm a first year student at the Ede Christian University of Applied Sciences</p>
+          <p className='text'>My name is Alan Osefia  and i'm a first year student at the Ede Christian University of Applied Sciences
+          I'm studying to become a software engineer,    </p>
+      
+          
 
         </div>
 
