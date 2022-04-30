9920989ae9d71a76b46e3bcdcaad3fe2efb0f5f5
20alan01 <20alan01@gmail.com>
2022-04-30 15:33:05 +0200

    contact form added

---
+++ pages/contact.jsx
@@ -0,0 +1,17 @@
+
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Navbar/Footer'
+
+export default function Stocks() {
+  return (
+    <>
+    <Navbar />
+    <h1>Stocks</h1>
+    <h2>Coming soon</h2>
+    <Footer />
+    </>
+  )
+}
