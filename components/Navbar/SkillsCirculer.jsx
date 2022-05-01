1ba8cabefe3360b8d075f88e72320d12214d2961
Alan Osefia <amosefia@student.che.nl>
2022-05-01 14:52:18 +0200

    logo's added to skills page

---
+++ components/Navbar/SkillsCirculer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+export default function SkillsCirculer() {
+    return (
+        <div className='SkillsCirculer skills-logo '>
+
+            <ul className="skills-list circle-list-items">
+                <li id='circle-item-1' className=' '>
+                    <i className="devicon-go-original-wordmark"></i>
+                </li>
+
+                <li id='circle-item-2'>
+                    <i class="devicon-csharp-plain"></i>
+                </li>
+                <li id='circle-item-3'>
+                    <i class="devicon-react-original"></i>
+                </li>
+
+                <li id='circle-item-4'
+                ><i class="devicon-mysql-plain-wordmark"></i>
+                </li>
+
+                <li id='circle-item-5'>
+
+                    <i class="devicon-html5-plain"></i>
+
+                </li>
+                <li id='circle-item-6'>
+                    <i class="devicon-css3-plain"></i>
+                </li>
+                <li id='circle-item-7'>
+                    <i class="devicon-nextjs-original"></i>
+                </li>
+                <li id='circle-item-8'>
+                    <i class="devicon-javascript-plain"></i>
+                </li>
+
+            </ul>
+        </div>
+    )
+}
--- pages/skills.jsx
+++ pages/skills.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Navbar/Footer'
+import SkillsCirculer from '../components/Navbar/SkillsCirculer'
 
 export default function Skills() {
   return (
@@ -11,15 +12,17 @@ export default function Skills() {
       <Navbar />
       <div className='hometext container text'>
         <div>
+       
           <h1>Tech Stack & Skills</h1>
           <p>I have some experience with MySQL workbench, GO, HTML & CSS and i'm currently developing my skills with: React, Next.JS, C# and Javascript</p>
+          
           
           <h2>Coming soon...</h2>
 
         </div>
 
       </div>
-
+     <SkillsCirculer />  
       <Footer />
     </>
   )
