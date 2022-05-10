99e8590bf25d835854c41eaa82f74e7a8d0853a4
20alan01 <20alan01@gmail.com>
2022-05-10 16:12:44 +0200

    Trying to link my projects, not yet finished

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Navbar/Footer'
-
+import Link from 'next/link';
 export default function Projects() {
   return (
     <>
@@ -25,10 +25,12 @@ export default function Projects() {
           <div className="container">
 
             <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+            <Link href="/projects">
+              <article>
               <div className="col">
                 <div className="card shadow-sm">
                   <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
                   <div className="card-body">
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <div className="d-flex justify-content-between align-items-center">
@@ -41,6 +43,8 @@ export default function Projects() {
                   </div>
                 </div>
               </div>
+              </article>
+              </Link>
               <div className="col">
                 <div className="card shadow-sm">
                   <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
